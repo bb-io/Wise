284d95e604a9525615d05c3dc37a8d571617fba2
vitalii-bezuhlyi <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2024-04-16 18:50:31 +0300

    Categories (#10)
    * CI: update workflow to invoke DEV deployment
    
    * CI: update pipeline to include dispatch event
    
    * CI: invoke Dev workflow once manually triggered
    
    * added categories
    
    * Updated csproj
    
    ---------
    
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>
    Co-authored-by: bZverok <137277669+bZverok@users.noreply.github.com>

--- Apps.Wise/WiseApplication.cs
+++ Apps.Wise/WiseApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Wise;
 
-public class WiseApplication : IApplication
+public class WiseApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.Fintech];
+        set { }
+    }
+    
     public string Name
     {
         get => "Wise";
